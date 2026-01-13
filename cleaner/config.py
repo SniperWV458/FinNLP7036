c371da5d2567693bdeb2318b425b5f54a01d83ac
xenon517 <100119186+xenon517@users.noreply.github.com>
2026-01-13 21:59:29 +0800

    config.py

--- cleaner/config.py
+++ cleaner/config.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 13 20:17:17 2026
-
-@author: 25010
-"""
-
 # 数据清洗配置参数
 CLEANING_CONFIG = {
     # 基本清洗参数
@@ -12,10 +5,12 @@ CLEANING_CONFIG = {
     'max_words': 500,                  # 最大词数
     'english_threshold': 0.7,          # 英语内容阈值
     'letter_ratio_threshold': 0.7,     # 字母占比阈值
+    'tag_ratio_threshold': 0.9,        # tag占比阈值（大于90%的数据将被剔除）
     
     # 文本处理参数
     'allowed_chars': 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 %+..?!$',  # 允许的字符
     'web_keywords': ['http', 'www', '.com', '.org', 'href=', '<div', '<p', '<br', '&amp;', '&#39;'],  # 网页关键词
+    'tag_pattern': r'\$[^\d\s]+',      # 匹配$后接非数字内容的tag
     
     # 输出格式
     'output_prefix': '[STOCKTWITS]',   # 输出前缀
